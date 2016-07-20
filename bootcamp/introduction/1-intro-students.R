fbd398df0e46f61cea0f2eb772c824806ae68d7b
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-07-20 10:20:06 -0400

    adding intro .R file

---
+++ bootcamp/introduction/1-intro-students.R
@@ -0,0 +1,78 @@
+# Session 1: An introduction to statistical programming
+
+################
+# Getting Help #
+################
+# Notes: 
+
+
+
+
+######################
+# Set Your Directory #
+######################
+# Notes: 
+
+
+
+
+#####################
+# R as a Calculator #
+#####################
+# Notes: 
+
+
+
+
+##################
+# Simple Objects #
+##################
+# Notes: 
+
+
+
+
+#########################
+# Workspace Environment #
+#########################
+# Notes: 
+
+
+
+
+###########
+# Vectors #
+###########
+# Notes: 
+
+
+
+
+#################
+# Vectorization #
+#################
+# Notes:
+
+
+
+
+#########################
+# Working with Packages #
+#########################
+# Notes:
+
+
+
+
+##################
+# Using Packages #
+##################
+# Notes:
+
+
+
+
+
+
+
+
