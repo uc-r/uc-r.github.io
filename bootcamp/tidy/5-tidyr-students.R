8775c50c8f3addfffe91f66a591e8f06151fd64e
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-07-29 15:40:57 -0400

    Update 5-tidyr-students.R

--- bootcamp/tidy/5-tidyr-students.R
+++ bootcamp/tidy/5-tidyr-students.R
@@ -1,15 +1,16 @@
 # Session 5: Creating tidy data sets with tidyr
 
 
-
 ################
 # Requirements #
 ################
 # install.packages("tidyr")
-library(tidyr)
+# install.packages("dplyr")
+# devtools::install_github("rstudio/EDAWR")
 
-install.packages("EDAWR")
-library(EDAWR)
+library(tidyr)
+library(dplyr)  # to illustrate pipe function
+library(EDAWR)  # for the data
 
 # data used (from the EDAWR package)
 cases
@@ -21,7 +22,6 @@ storms
 #####################
 # Notes: 
 
-library(dplyr)
 
 # filter(data, variable == numeric_value) is the same as...
 # data %>% filter(variable == numeric_value)
@@ -58,6 +58,19 @@ gather(data = cases, key = Year, value = n, 2:4)
 cases %>% gather(Year, n, 2:4)
 
 
+
+##############
+# YOUR TURN! #
+##############
+# Import the `expenditures.csv` file in the data folder
+
+
+# Reshape this data from wide to long
+
+
+
+
+
 ############
 # spread() #
 ############
@@ -73,6 +86,19 @@ spread(data = wide, key = Year, value = n)
 wide %>% spread(Year, n)
 
 
+
+##############
+# YOUR TURN! #
+##############
+# Create the following data frame
+long <- expenditures %>% gather(Year, Costs, 2:15)
+
+# Reshape this data from long to wide
+
+
+
+
+
 ##############
 # separate() #
 ##############
@@ -82,6 +108,20 @@ wide %>% spread(Year, n)
 # these two options are the same
 separate(data = storms, col = date, into = c("year", "month", "day"), sep = "-")
 storms %>% separate(date, c("year", "month", "day"), sep = "-")
+
+
+
+##############
+# YOUR TURN! #
+##############
+# using the original expenditures data...
+
+# 1. Reshape this data from wide to long
+
+
+# 2. Separate FY from the Year
+
+
 
 
 
@@ -98,3 +138,15 @@ sep_col
 # these two options work the same
 unite(data = sep_col, col = date, year, month, day, sep = "-")
 sep_col %>% unite(date, year, month, day, sep = "-")
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Import the facebook.tsv file in the data folder
+
+
+# Combine the `Period` and `Year` variables
+
+
