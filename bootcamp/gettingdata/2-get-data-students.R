2d2797dc8480a5e1205152bafe3b9cce4d993f00
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-07-29 15:38:56 -0400

    Update 2-get-data-students.R

--- bootcamp/gettingdata/2-get-data-students.R
+++ bootcamp/gettingdata/2-get-data-students.R
@@ -8,6 +8,17 @@
 
 
 
+##############
+# YOUR TURN! #
+##############
+# 1. Load the iris data set
+
+
+# 2. What is this data measuring?
+
+
+
+
 ########################
 # Importing Text Files #
 ########################
@@ -16,10 +27,39 @@
 
 
 
+##############
+# YOUR TURN! #
+##############
+# 1. Read in the facebook.tsv file in the data folder
+
+
+# 2. Read in and save the facebook.tsv file as an object titled facebook
+
+
+# 3. Take a peek at what this data looks like
+
+
+
+
 #########################
 # Importing Excel Files #
 #########################
-# Notes: 
+# Notes: you need to install the readxl package to read in Excel files
+
+
+
+
+##############
+# YOUR TURN! #
+##############
+# 1. Read in the spreadsheet titled "3. Median HH income, metro" in the 
+# "PEW Middle Class Data.xlsx" file
+
+
+# 2. Save it as an object titled pew
+
+
+# 3. Take a peek at what this data looks like
 
 
 
@@ -29,6 +69,31 @@
 #########################
 # Notes: 
 
+# scrape this url .csv file
+url <- "https://www.data.gov/media/federal-agency-participation.csv"
+
+
+
+# scrape this url .xls file
+# you'll need to install the gdata package if you have not already done so
+install.packages("gdata")
+library(gdata)
+
+url <- "http://www.huduser.org/portal/datasets/fmr/fmr2015f/FY2015F_4050_Final.xls"
+
+
+
+
+##############
+# YOUR TURN! #
+##############
+# 1. Download the file stored at: https://bradleyboehmke.github.io/public/data/reddit.csv
+
+
+# 2. Save it as an object titled reddit
+
+
+# 3. Take a peek at what this data looks like
 
 
 
