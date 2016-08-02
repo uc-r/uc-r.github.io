a8bfee6461f5ef374d6418c83c19a8cb2ba2b419
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-08-02 10:59:01 -0400

    Create 10-case-study-2.R

---
+++ bootcamp/case_studies/10-case-study-2.R
@@ -0,0 +1,26 @@
+# Download the most recent Occupational Employment Statistics data (May 2015)
+# from https://www.dropbox.com/s/5cagoqaa67uix3e/all_data_M_2015.xlsx?dl=0
+
+
+
+# How does Cincinnati rank nationally for median annual wage across all occupations & industries?
+# Note, you only want to compare Cincinnati to other Metropolitan Statistical Areas
+
+
+
+
+# How does Cincinnati rank among the following locations for median annual wage and
+# also total employment
+
+
+
+
+# Identify the top 25 occupations that employ the most people in Cincinnati
+
+
+
+
+# Do these occupations provide the top paying salaries? If not, which occupations do?
+
+
+
