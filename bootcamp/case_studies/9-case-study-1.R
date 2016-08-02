6f28a7cbae00c0b13d26e5379a450b1e85e41b08
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-08-02 09:22:17 -0400

    Create 9-case-study-1.R

---
+++ bootcamp/case_studies/9-case-study-1.R
@@ -0,0 +1,23 @@
+# Download the Police Crime Incident Data at 
+# https://www.dropbox.com/s/vc4ldivb2op7phl/crime.csv?dl=0
+
+
+
+# What are the top 25 most prevelant offenses in this data set? What are some
+# issues you are finding with the coding of this data?
+
+
+
+
+# Have the rate of occurrence for the top 10 offenses increasing or decreasing
+# since 2010?
+
+
+
+
+# What are the top 10 most prevelant offenses so far this year?
+
+
+
+
+# What neighborhoods are these offenses most often occuring in?
