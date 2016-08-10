e086eae71ca271cbbbbbd60a310cc704c6ffbd20
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-08-10 06:58:30 -0400

    Update 7-quick-plotting-students.R

--- bootcamp/quickplots/7-quick-plotting-students.R
+++ bootcamp/quickplots/7-quick-plotting-students.R
@@ -27,7 +27,6 @@ supermarket <- read_excel("data/Supermarket Transactions.xlsx", sheet = "Data")
 ###############
 # Strip Chart #
 ###############
-# Notes:
 
 # default
 stripchart(mtcars$mpg)
@@ -40,13 +39,16 @@ stripchart(facebook$tenure, pch = 16)
 #############
 # Histogram #
 #############
-# Notes:
 
 # default
 hist(facebook$tenure)     
 
 # add different parameters to change breaks, color, title, etc.
-hist()
+hist(facebook$tenure, breaks = 100, col = "grey", 
+     main = "Facebook User Tenure", xlab = "Tenure (Days)")
+
+# get probabilities versus counts by entering probability = TRUE
+hist(facebook$tenure, breaks = 100, col = "grey", probability = TRUE)
 
 # you can also compare the histogram to a normal curve to see how it deviates
 # there's a lot going on here so this is just for your reference
@@ -56,7 +58,7 @@ x <- na.omit(facebook$tenure)
 
 ## create the histogram
 h <- hist(x, breaks = 100, col = "grey", main = "Facebook User Tenure", 
-        xlab = "Tenure (Days)")
+          xlab = "Tenure (Days)")
 
 ## create normal curve data
 xfit <- seq(min(x), max(x), length = 100) 
@@ -71,33 +73,33 @@ lines(xfit, yfit, col = "red", lwd = 2)
 ################
 # Density Plot #
 ################
-# Notes:
 
 # first you need to calculate the density (be sure to remove missing data with
 # na.rm = TRUE)
 d <- density(facebook$tenure, na.rm = TRUE)
 
 # you can now use plot() to plot the density data
-plot()
+plot(d)
 
 # you can fill area under the curve with polygon()
-polygon()
+polygon(d, col = "red", border = "blue")
 
 
 
 ############
 # Box Plot #
 ############
-# Notes:
 
 # default
 boxplot(facebook$tenure)
 
 # change to horizontal
-boxplot()
+boxplot(facebook$tenure, horizontal = TRUE)
 
 # add notches
-boxplot()
+boxplot(facebook$tenure, horizontal = TRUE, notch = TRUE, col = "grey40")
+boxplot(mtcars$mpg, horizontal = TRUE, notch = TRUE, col = "grey40") # better example
+
 
 # you can also add mean value points but you can only do it to vertical boxplots
 boxplot(mtcars$mpg, notch = TRUE)
@@ -108,14 +110,18 @@ points(mean(mtcars$mpg), pch=18, col = "red")
 #############
 # Your Turn #
 #############
-# Using the facebook data visually assess the continous variables. What insights
-# do you find?
+# Using the facebook data visually assess the continous variables. 
+# What insights do you find?
 
+# Example 1 - age of users
+hist(facebook$age, breaks = 100, col = "grey", main = "Age of Facebook Users", 
+     xlab = "Age (Years)")
 
+# Example 2 - friend count of users
+hist(facebook$friend_count, breaks = 100, col = "grey")
+hist(log10(facebook$friend_count), breaks = 100, col = "grey")
 
-
-
-
+# Example 3 - anyone want to share?
 
 
 #############
@@ -132,7 +138,10 @@ barplot(table(reddit$dog.cat))
 
 # include and/or change features such as title, horizontal alignment, category
 # names, color
-barplot()
+pets <- table(reddit$dog.cat)
+par(las = 1)
+barplot(pets, main = "Reddit User Animal Preferences", horiz = TRUE, 
+        names.arg = c("Cats", "Dogs", "Turtles"), col = 'cyan')
 
 # plot this data
 library(dplyr)
@@ -143,7 +152,8 @@ state <- reddit %>%
         arrange(n) %>%
         filter(state != "")
 
-
+par(mar = c(3,8,1,1), las = 1)
+barplot(state$n, names.arg = state$state, horiz = TRUE)
 
 
 ############
@@ -152,10 +162,7 @@ state <- reddit %>%
 # Notes:
 
 # now plot the same state data with a dot plot
-dotchart()
-
-
-
+dotchart(state$n, labels = state$state, cex = .7)
 
 
 
@@ -164,14 +171,27 @@ dotchart()
 #############
 # Using the Reddit data... 
 
-# 1) assess the frequency of education levels (note: preceed your plot function 
-# with par(mar = c(5,15,1,1), las = 2))
+# 1) assess the frequency of education levels
+reddit_ed <- reddit %>%
+        group_by(education) %>%
+        tally() %>%
+        filter(education != "NA") %>%
+        arrange(n)
 
-
+par(mar = c(5,15,1,1), las = 1)
+barplot(reddit_ed$n, names.arg = reddit_ed$education, horiz = TRUE)
 
 # 2) assess how the different cheeses rank with Reddit users. What do you find?
+cheese <- reddit %>%
+        group_by(cheese) %>%
+        tally() %>%
+        filter(cheese != "NA") %>%
+        arrange(n)
 
+dotchart(cheese$n, labels = cheese$cheese, bg = "yellow")
 
+# reset margins
+par(mar = c(5, 4, 4, 2))
 
 
 
@@ -188,8 +208,8 @@ plot(x = race$Black_unemployment, y = race$black_college)
 # you can fit lines to the scatter plot to assess its linearity; note that you 
 # need to use "~" rather than "x =" and "y ="
 plot(White_unemployment ~ Black_unemployment, data = race)
-abline()
-lines()
+abline(lm(White_unemployment ~ Black_unemployment, data = race), col = "red")
+lines(lowess(race$White_unemployment ~ race$Black_unemployment), col = "blue")
 
 # quickly assess multiple scatter plots with pairs()
 pairs(race)
@@ -231,30 +251,36 @@ legend("topleft", legend = c("HS Rate", "College Rate", "Unemployment"),
 
 # turn this single variable boxplot into two box plots comparing gender
 boxplot(supermarket$Revenue)
-boxplot()
+boxplot(Revenue ~ Gender, data = supermarket)
 
 # add a third variable for marital status
-boxplot()
+boxplot(Revenue ~ Gender + `Marital Status`, data = supermarket)
 
 
 
 #############
 # Your Turn #
 #############
-# Using the supermarket data, analyze revenue by date, homeownership, city, product 
+# Using the supermarket data analyze revenue by date, homeownership, city, product 
 # family, etc.  Don't forget you can summarize the data using dplyr like you
 # learned about earlier...example:
 
-## this sums total sales by data
-supermarket %>%
+## total revenues by date
+revenue_by_date <- supermarket %>%
         group_by(`Purchase Date`) %>%
         summarise(Revenue = sum(Revenue, na.rm = TRUE))
 
+plot(Revenue ~ `Purchase Date`, data = revenue_by_date, type = "l", col = "grey")
+lines(lowess(revenue_by_date$Revenue ~ revenue_by_date$`Purchase Date`, f = 1/4), col = "blue")
 
 
+## revenue by product category
+par(mar = c(5, 10, 4, 2))
+boxplot(Revenue ~ `Product Category`, data = supermarket, horizontal = TRUE)
 
 
-
+# reset margins
+par(mar = c(5, 4, 4, 2))
 
 ######################
 # Bar Charts...again #
@@ -272,8 +298,8 @@ barplot(counts, col = c("darkblue", "red"), legend = c("Married", "Single"), bes
 proportions <- prop.table(counts)
 
 # now plot these results in a similar manner as above
-barplot()
-
+barplot(proportions, col = c("darkblue", "red"), legend = c("Married", "Single"))
+barplot(proportions, col = c("darkblue", "red"), legend = c("Married", "Single"), beside = TRUE)
 
 
 #############
@@ -281,16 +307,30 @@ barplot()
 #############
 # Using the reddit data compare counts of...
 # 1) product family by homeownership
+Q1 <- table(supermarket$`Product Family`, supermarket$Homeowner)
+
+barplot(Q1, beside = TRUE, legend.text = TRUE)
 
 
 # 2) annual income by homeownership
+supermarket$`Annual Income` <- factor(supermarket$`Annual Income`, 
+                                      levels = c("$10K - $30K", "$30K - $50K",
+                                                 "$50K - $70K", "$70K - $90K",
+                                                 "$90K - $110K", "$110K - $130K",
+                                                 "$130K - $150K", "$150K +"))
 
+Q2 <- prop.table(table(supermarket$`Annual Income`, supermarket$Homeowner))
+
+barplot(Q2, legend.text = TRUE, 
+        args.legend = list(x = "topleft", cex = 1, bty = "n", y.intersp=1.5))
 
 # 3) country by gender
+Q3 <- prop.table(table(supermarket$Gender, supermarket$Country))
 
+barplot(Q3, beside = TRUE, legend.text = c("Female", "Male"), col = c("pink", "blue"),
+        args.legend = list(x = "topleft", cex = 1.5, bty = "n", y.intersp=1.5))
 
 # 4) etc.
-
 
 
 
