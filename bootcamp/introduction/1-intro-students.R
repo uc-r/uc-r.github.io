eaa4fe51df1ec92870ef305919984290714555a3
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-08-10 06:42:23 -0400

    Update 1-intro-students.R

--- bootcamp/introduction/1-intro-students.R
+++ bootcamp/introduction/1-intro-students.R
@@ -1,87 +1,116 @@
 # Session 1: An introduction to statistical programming
 
-# Place a hashtag ("#") at the beginning of any line that is just a note; this
-# will keep R from running these lines as code. Any lines without a hashtag
-# will be assessed when sourced
-
-2 + 2
-
 ################
 # Getting Help #
 ################
-# Notes: 
 
-
-
+help("sum")
+?mean
+example("sum")
 
 ######################
 # Set Your Directory #
 ######################
-# Notes: 
 
+getwd()
 
+# Exercise: set your working directory using setwd()
 
 
 #####################
 # R as a Calculator #
 #####################
-# Notes: 
 
-
+4 + 3 / 10 ^ 2
+4 + (3 / 10 ^ 2)
+(4 + 3) / 10 ^ 2
+1 / 17 ^ 7
+1 / 0
+Inf - Inf
 
 
 ##################
 # Simple Objects #
 ##################
-# Notes: 
 
+x <- 3                  # assign 3 to x
+x                       # evaluate x
+
+x <- x + 1              # we can increment (build onto) existing objects
+x
+
+x = 3                   # BAD
+x <- 3                  # GOOD
+
+X                       # case sensitive
 
 
 ##############
 # YOUR TURN! #
 ##############
 # Calculate Q in the in the economic order quantity model presented
+D <- 1000
+K <- 5
+h <- .25
 
-
+Q <- sqrt((2 * D * K) / h)
+Q
 
 
 
 #########################
 # Workspace Environment #
 #########################
-# Notes: 
 
-
+ls()                    # list objects in your global environment
+rm(D)                   # remove defined object
+rm(list = ls())         # remove all objects
 
 
 ###########
 # Vectors #
 ###########
-# Notes: 
 
+1:10
+-3:5
 
+x <- 1:10
+y <- c(2, 5, -1)
 
 
 #################
 # Vectorization #
 #################
-# Notes:
 
+x <- c(1, 3, 4) 
+y <- c(1, 2, 4)
 
+x + y
+x * y
+x > y
+
+long <- 1:10
+short <- 1:4
+
+long + short
 
 
 ##############
 # YOUR TURN! #
 ##############
 # Calculate Q in the in the economic order quantity model presented
+D <- 1000
+K <- 5
+h <- c(.25, .50, .75)
 
+Q <- sqrt((2 * D * K) / h)
+Q
 
 
 
 #########################
 # Working with Packages #
 #########################
-# Notes:
 
 
 
@@ -89,7 +118,12 @@
 ##############
 # YOUR TURN! #
 ##############
-# Download the packages listed on the slide
+# Download the packages listed
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("ggplot2")
+install.packages("stringr")
+install.packages("lubridate")
 
 
 
@@ -97,9 +131,11 @@
 ##################
 # Using Packages #
 ##################
-# Notes:
 
-
+library(dplyr)                                  # activate package
+help(package = "dplyr")                         # provides details regarding package
+vignette(package = "dplyr")                     # list vignettes available for a package
+vignette("introduction", package = "dplyr")     # view specific vignette
 
 
 
